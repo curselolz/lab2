e92899e0a5a1c28fa0bb647352279a1d965be6b8
Oleksandr <vimakek321@mmgaklan.com>
2023-09-25 17:07:50 +0300

    Added a comment

--- hello.java
+++ hello.java
@@ -1,6 +1,7 @@
 public class Hello { 
     public static void main( String argv[] ) 
     { 
+        // Default is "World" 
         String name = "World"; 
         if ( argv.length != 0 ) 
         { 
@@ -8,4 +9,4 @@ public class Hello {
         } 
         System.out.println( "Hello, " + name + "!" ); 
     } 
-}  
+} 
